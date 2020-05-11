6500f54c226f6fd72269a7ab737135673b8872ed
zz070 <2389655715@qq.com>
2020-05-11 19:07:41 +0800

    选择排序

--- Test/src/Sort/Select.java
+++ Test/src/Sort/Select.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Select {
     public static void selectSort(int[] array){
         //升序排序
-        //[0,array.length - i)是未排序区域，[array.length - i,array.length)是已排序区域
+        //[0,array.length - bound)是未排序区域，[array.length - bound,array.length)是已排序区域
         //以bound位置元素作为擂主，打擂成功就交换位置
         for(int bound = 0;bound<array.length-1;bound++){
             for(int cur = bound+1;cur<array.length;cur++){
@@ -17,6 +17,7 @@ public class Select {
             }
         }
     }
+
     public static void main(String[] args) {
         int[] array = {1,8,6,3,0,9,2,4,7,5};
         System.out.print("排序前：");
