b1556287bfb6b0015a5c14131500e60c01383023
zz070 <2389655715@qq.com>
2020-05-11 22:48:39 +0800

    冒泡排序

--- Test/src/Sort/Bubble.java
+++ Test/src/Sort/Bubble.java
@@ -14,6 +14,23 @@ public class Bubble {
             }
         }
     }
+    public static void bubbleSort1(int[] array) {
+        // 按照每次找最小的方式来进行排序. (从后往前比较交换)
+        for (int bound = 0; bound < array.length; bound++) {
+            // [0, bound) 已排序区间
+            // [bound, size) 待排序区间
+            // cur > bound 而不是 >= , 当 bound 为 0 的时候, 如果 >= , cur 也为 0, cur - 1 也就下标越界了
+            for (int cur = array.length - 1; cur > bound; cur--) {
+                // 此处 cur - 1 是因为 cur 初始值是 array.length - 1. 如果取 cur + 1 下标的元素, 就越界了
+                // 此处的条件如果写成 >= 同样无法保证稳定性
+                if (array[cur - 1] > array[cur]) {
+                    int t=array[cur];
+                    array[cur]=array[cur-1];
+                    array[cur-1] = t;
+                }
+            }
+        }
+    }
     public static void main(String[] args) {
         int[] array = {1,8,6,3,0,9,2,4,7,5};
         System.out.print("排序前：");
