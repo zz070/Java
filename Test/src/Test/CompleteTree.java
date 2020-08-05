d723a675c72bc34d5d197f8e03d7f3e259ce0599
zz070 <2389655715@qq.com>
2020-08-05 23:20:31 +0800

    完全二叉树

---
+++ Test/src/Test/CompleteTree.java
@@ -0,0 +1,46 @@
+package Test;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class TreeNode {
+    int val;
+    Tree.TreeNode left;
+    Tree.TreeNode right;
+
+    public TreeNode(int val){
+        this.val = val;
+    }
+}
+public class CompleteTree {
+    public boolean isCompleteTree(TreeNode root){
+        if(root == null){
+            return true;
+        }
+        Queue<TreeNode> tree = new LinkedList<>();
+        tree.offer(root);
+        boolean second = false;
+        while(!tree.isEmpty()){
+            TreeNode cur = tree.poll();
+            if(!second){
+                if(cur.left != null && cur.right != null){
+                    tree.offer(cur.left);
+                    tree.offer(cur.right);
+                }else if(cur.left != null && cur.right == null){
+                    tree.offer(cur.left);
+                    second = true;
+                }else if(cur.left == null && cur.right != null){
+                    return false;
+                }else{
+                    second = true;
+                }
+            }else{
+                if(cur.left != null || cur.right != null){
+                    return false;
+                }
+            }
+        }
+        return  true;
+    }
+}
